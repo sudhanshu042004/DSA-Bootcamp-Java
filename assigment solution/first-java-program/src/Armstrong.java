01bfd598b0ae466eeec0350be07e85fb12896e10
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-08-21 17:59:44 +0530

    added Armstrong no.

--- assigment solution/first-java-program/src/Armstrong.java
+++ assigment solution/first-java-program/src/Armstrong.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 public class Armstrong {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Give two to find Armstrong number between them: ");
         int a = input.nextInt();
         int b = input.nextInt();
-        for (int c = a; c < b; c++ ) {
+        for (int c = a; c <= b; c++){
             String stringC = String.valueOf(c);
-            int lengthC = stringC.length();
-//            System.out.println(lengthC);
-            int sum = c;
+             int length = stringC.length();
+             int sum = 0;
 
-            
-
+             for(int i= 0; i < length; i++ ){
+                 char character = stringC.charAt(i);
+                 int num = Integer.parseInt(String.valueOf(character));
+                 sum += Math.pow(num, length);
+             }
+             if (sum == c){
+                 System.out.println("Armstrong no.s :" + c);
+             }
         }
+
+
+
+
     }
 }

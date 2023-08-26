23333bd5820d27c6a764e547110bb0eae5f48e64
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-08-26 11:01:40 +0530

    added rhombus area program

---
+++ assigment solution/condition-loops/src/Rhombus.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Rhombus {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float q1 = in.nextFloat();
+        float q2 = in.nextFloat();
+        float area = (q1 * q2)/2;
+        System.out.println(area);
+    }
+}

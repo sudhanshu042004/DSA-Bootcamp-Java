6ba1cfc7c58ca95ccc43d4f0e0211efedd832d8b
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2023-09-05 17:33:10 +0530

    added Factor Program

---
+++ assigment solution/condition-loops/src/array.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        int[] rnos = new int[5];
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(arr));
+    }
+}
